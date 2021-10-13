01608c5371bb586057de8d7f0bf3db5c60314edd
Stackbit Code Editor <projects@stackbit.com>
2021-10-13 20:14:06 +0000

    tailwind.config.js: changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
     theme: {
         extend: {
             
-            fontFamily: {
+        fontFamily: {
         bebas: ['"Bebas Neue"', sans-serif],
       },
             colors: {
